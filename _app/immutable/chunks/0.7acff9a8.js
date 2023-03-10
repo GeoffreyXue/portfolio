29047be0e9571dc4d08b1bd5cbb3a8d67ade3363
Geoffrey Xue <xuegeoff@gmail.com>
2023-03-10 11:41:02 -0600

    Updates

---
+++ _app/immutable/chunks/0.7acff9a8.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_layout.svelte.d14580aa.js";export{t as component};
---
+++ _app/immutable/chunks/4.0b38fac1.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_page.da46b06b.js";import{default as t}from"../entry/resume-page.svelte.39a60607.js";export{t as component,r as universal};
