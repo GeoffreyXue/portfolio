aed4917132370e76e8aa33fc4c8783ac2bca4ad6
Geoffrey Xue <xuegeoff@gmail.com>
2023-03-09 18:37:15 -0600

    Updates

--- _app/immutable/chunks/1.9f8e4d74.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.26f52fc8.js";export{t as component};
---
+++ _app/immutable/chunks/stores.c19d72fd.js
@@ -0,0 +1,1 @@
+import"./index.d34bd15e.js";import{s as e}from"./singletons.b2c1567e.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
